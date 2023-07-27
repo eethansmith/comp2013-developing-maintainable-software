2d0e53ca2907729a1ed75b2d9e85d962138af916
Ethan Smith <psyes9@exmail.nottingham.ac.uk>
2023-07-27 16:35:23 +0100

    Uploading Source - Pacman.java

---
+++ Pacman.java
@@ -0,0 +1,16 @@
+package sample;
+
+
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Pacman extends Circle {
+
+    public Pacman(double x, double y) {
+        this.setCenterX(x);
+        this.setCenterY(y);
+        this.setRadius(25);
+        this.setFill(Color.YELLOW);
+    }
+}
