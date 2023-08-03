1283c20f801bfa228a347d5122263eade924b8d7
Ethan Smith <psyes9@exmail.nottingham.ac.uk>
2023-08-03 18:56:48 +0100

    In line comments made to meet Concise coding conventions

--- src/com/pacman/Obstacle.java
+++ src/com/pacman/Obstacle.java
@@ -9,12 +9,12 @@ import javafx.scene.shape.Rectangle;
  * Obstacle could be either horizontal or vertical.
  */
 public class Obstacle extends Rectangle {
-    
+
     /**
      * Static value for the thickness of the obstacle.
      */
     public static double THICKNESS = 25;
-    
+
     /**
      * Constructor for Obstacle class, sets the position, dimensions, and visual properties of the obstacle.
      *
@@ -24,16 +24,28 @@ public class Obstacle extends Rectangle {
      * @param length     The length of the obstacle, the unit is the thickness of the obstacle.
      */
     public Obstacle(double x, double y, String orientation, double length) {
+        // Set x-coordinate of obstacle's position
         this.setX(x);
+
+        // Set y-coordinate of obstacle's position
         this.setY(y);
+
+        // If orientation is horizontal, set height to THICKNESS and width to length times THICKNESS
         if (orientation.equals("horizontal")) {
             this.setHeight(Obstacle.THICKNESS);
             this.setWidth(length * Obstacle.THICKNESS);
-        } else {
+        }
+
+        // If orientation is not horizontal (i.e., vertical), set width to THICKNESS and height to length times THICKNESS
+        else {
             this.setHeight(length * Obstacle.THICKNESS);
             this.setWidth(Obstacle.THICKNESS);
         }
+        
+        // Set fill color of the obstacle to BLUE
         this.setFill(Color.BLUE);
+
+        // Set stroke width of the obstacle
         this.setStrokeWidth(3);
     }
 }
