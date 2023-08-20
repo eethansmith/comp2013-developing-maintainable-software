cc37a45fb7107569897b9ebdea447a1fff0fff94
Ethan Smith <psyes9@exmail.nottingham.ac.uk>
2023-08-20 04:11:17 +0100

    MainFX.java file created and communicates with the fxml file and javafx game

---
+++ src/main/java/example/MainFX.java
@@ -0,0 +1,32 @@
+package example;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage primaryStage) throws Exception {
+        primaryStage.setTitle("PACMAN ARCADE - MAIN MENU");
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.show();
+    }
+
+    public static void launchGame(Stage currentStage) {
+        Platform.runLater(() -> {
+            Main game = new Main();
+            game.startGame();
+            currentStage.hide();
+        });
+    }
+}
