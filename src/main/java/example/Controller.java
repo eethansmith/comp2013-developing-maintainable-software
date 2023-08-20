ab54b229f2d2d7b9a6e766bd24157fc4ea194976
Ethan Smith <psyes9@exmail.nottingham.ac.uk>
2023-08-20 04:10:05 +0100

    Controller.java code added for FXML file control

--- src/main/java/example/Controller.java
+++ src/main/java/example/Controller.java
@@ -1,4 +1,24 @@
 package example;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private void switchToGame(ActionEvent event) {
+        Stage currentStage = (Stage) playButton.getScene().getWindow();
+        MainFX.launchGame(currentStage);
+        System.out.println("Prepare to enter the game...");
+    }
+
+    public void selectLevel(MouseEvent mouseEvent) {
+        System.out.println("Level Select Clicked *Click*...");
+    }
 }
